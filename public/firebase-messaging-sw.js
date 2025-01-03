da4884cca57ca81ef3929dfb3948dbcc0a42f5a8
y0unj1NoH <nohyoong711@gmail.com>
2025-01-03 21:19:59 +0900

    fix: initialize 버그 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -13,15 +13,33 @@ if (typeof importScripts === "function") {
     console.log("FCM SW activated");
   });
 
+  const firebaseConfig = {
+    apiKey: "{API_KEY}",
+    authDomain: "{AUTH_DOMAIN}",
+    projectId: "{PROJECT_ID}",
+    storageBucket: "{STORAGE_BUCKET}",
+    messagingSenderId: "{MESSAGING_SENDER_ID}",
+    appId: "{APP_ID}",
+  };
+
   const firebaseApp = firebase.initializeApp({
-    apiKey: process.env.VITE_FCM_API_KEY,
-    authDomain: process.env.VITE_FCM_AUTH_DOMAIN,
-    projectId: process.env.VITE_FCM_PROJECT_ID,
-    storageBucket: process.env.VITE_FCM_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_FCM_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_FCM_APP_ID,
-    measurementId: process.env.VITE_FCM_MEASUREMENT_ID,
+    apiKey: "{API_KEY}",
+    authDomain: "{AUTH_DOMAIN}",
+    projectId: "{PROJECT_ID}",
+    storageBucket: "{STORAGE_BUCKET}",
+    messagingSenderId: "{MESSAGING_SENDER_ID}",
+    appId: "{APP_ID}",
+    measurementId: "{MEASUREMENT_ID}",
   });
+  // const firebaseApp = firebase.initializeApp({
+  //   apiKey: process.env.VITE_FCM_API_KEY,
+  //   authDomain: process.env.VITE_FCM_AUTH_DOMAIN,
+  //   projectId: process.env.VITE_FCM_PROJECT_ID,
+  //   storageBucket: process.env.VITE_FCM_STORAGE_BUCKET,
+  //   messagingSenderId: process.env.VITE_FCM_MESSAGING_SENDER_ID,
+  //   appId: process.env.VITE_FCM_APP_ID,
+  //   measurementId: process.env.VITE_FCM_MEASUREMENT_ID,
+  // });
 
   firebase.messaging(firebaseApp);
 
