d4370494ae0ada78f8db2e3fec8526702d360ff3
y0unj1NoH <nohyoong711@gmail.com>
2025-01-03 20:06:21 +0900

    fix: yarn gen-pwa 미실행 버그 해결

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["sharp"],
+    },
+  },
   plugins: [
     react(),
     VitePWA({
