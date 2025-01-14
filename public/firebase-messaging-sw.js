fd855465b2fd19c8af3d4e5f69d8afbc489c0fb5
y0unj1NoH <nohyoong711@gmail.com>
2025-01-14 14:54:43 +0900

    feat: 알림 icon 상대 경로로 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -50,7 +50,7 @@ if (typeof importScripts === "function") {
     if (notificationTitle) {
       const notificationOptions = {
         body: payload.body,
-        icon: "/favicon-32x32.png",
+        icon: "./favicon-32x32.png",
       };
       self.registration.showNotification(
         notificationTitle,
